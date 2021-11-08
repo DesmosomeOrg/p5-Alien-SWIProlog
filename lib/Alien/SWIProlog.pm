60417c0dc61ad8cdaad4e416890303b56da13c6e
Zakariyya Mughal <zaki.mughal@gmail.com>
2021-11-08 01:07:29 -0500

    Initial commit

---
+++ lib/Alien/SWIProlog.pm
@@ -0,0 +1,14 @@
+package Alien::SWIProlog;
+# ABSTRACT: Alien package for the SWI-Prolog Prolog interpreter
+
+use strict;
+use warnings;
+
+use parent qw(Alien::Base);
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Alien-SWIProlog/>
+
+=cut
